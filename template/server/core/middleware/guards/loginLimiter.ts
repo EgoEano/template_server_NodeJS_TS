cebce5b48402df39660587bfbb106473522b4596
ErsteMaster <testo@testo.tt>
2026-02-01 15:53:56 +0500

    1553-010226

--- template/server/core/middleware/guards/loginLimiter.ts
+++ template/server/core/middleware/guards/loginLimiter.ts
@@ -1,11 +1,11 @@
 import { RateLimiterRedis } from 'rate-limiter-flexible';
-import type { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction, RequestHandler } from 'express';
 
 import Logger from '../loggers/loggerService.js';
 import { redisLegacyPool } from '../../services/connection/pool.js';
 
 interface LoginBody {
-    login: string | number;
+    login: string;
 }
 
 type RequestWithLogin = Request<
@@ -13,6 +13,8 @@ type RequestWithLogin = Request<
     Record<string, never>, // res body
     LoginBody               // req body
 >;
+
+type LoginRequest = Request<Record<string, string>, unknown, LoginBody>;
 
 const limiter = new RateLimiterRedis({
     storeClient: redisLegacyPool.getClient(),
@@ -22,27 +24,35 @@ const limiter = new RateLimiterRedis({
     blockDuration: 60 * 15, // 15 min
 });
 
-export async function loginLimiter(
-    req: RequestWithLogin, res: Response, next: NextFunction
-) {
-    const { login } = req.body;
-    if (!login) {
-        Logger.warn({ message: 'Blocked request without login', source: 'loginLimiter' });
-        return res.status(400).json({ message: 'login is not detected' });
-    }
 
-    try {
-        await limiter.consume(login);
-        next();
-    } catch (err: unknown) {
-        Logger.error({ message: 'Too many request', source: 'loginLimiter', error: err });
-        res.status(429).json({ message: 'Too many request, please try later' });
-    }
-}
+export const loginLimiter: RequestHandler<
+    Record<string, string>, // params
+    unknown, // res body
+    LoginBody // req body
+> = async (
+    req: LoginRequest,
+    res: Response,
+    next: NextFunction
+) => {
+        const { login } = req.body;
+        if (!login) {
+            Logger.warn({ message: 'Blocked request without login', source: 'loginLimiter' });
+            return res.status(400).json({ message: 'login is not detected' });
+        }
 
-export async function deleteLimiter(
+        try {
+            await limiter.consume(login);
+            next();
+        } catch (err: unknown) {
+            Logger.error({ message: 'Too many request', source: 'loginLimiter', error: err });
+            return res.status(429).json({ message: 'Too many request, please try later' });
+        }
+    };
+
+
+export const deleteLimiter = async (
     req: RequestWithLogin
-) {
+) => {
     const { login } = req.body;
     if (login) {
         try {
